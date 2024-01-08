58238f31aa70361d267e8b96791df74151c351e6
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-01-08 02:45:04 -0300

    feat(app): add initial screen page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule],
   templateUrl: './app.component.html',
-  styles: [],
 })
 export class AppComponent {
-  title = 'betun-web';
+  protected readonly title = 'betun-web';
 }
