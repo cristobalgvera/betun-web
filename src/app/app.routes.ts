2b3e3b9e81972a0921609f13586e95d10b8ec128
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-01-11 03:43:27 -0300

    feat(home): add base home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { homeRoutes } from '@pages/home';
 import { playersRoutes } from '@pages/players';
 
-export const routes: Routes = [{ path: 'players', children: playersRoutes }];
+export const routes: Routes = [
+  { path: '', children: homeRoutes },
+  { path: 'players', children: playersRoutes },
+  { path: '**', redirectTo: '' },
+];
